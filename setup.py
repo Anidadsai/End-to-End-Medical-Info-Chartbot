ce8c542e00434c0df33faad3a5e3d09d2d11a4f6
Anidadsai <ani.da.ds.ai@gmail.com>
2025-08-09 16:13:39 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Generative AI Project",
+    version="0.0.0",
+    author="Anil Wadeda",
+    author_email="ani.da.ds.ai@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
